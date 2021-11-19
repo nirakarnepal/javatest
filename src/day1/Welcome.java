6351049a5fe4526f17f74f4ec62e2143071cebd9
Nirakar Nepal <nnepal@verra.org>
2021-11-18 19:50:41 -0600

    Print current date and time

--- src/day1/Welcome.java
+++ src/day1/Welcome.java
@@ -2,10 +2,15 @@ package day1;
 
 public class Welcome {
 
+    //function to print current date and time
+    public static void printDateTime() {
+        java.util.Date date = new java.util.Date();
+        System.out.println(date.toString());
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-
+        printDateTime();
     }
 }
 
