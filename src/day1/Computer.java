efcc4352c5ae28f8b583bd9cb61d9a7076eff7de
Nirakar Nepal <nnepal@verra.org>
2021-11-23 19:05:05 -0600

    Class Computer Added

---
+++ src/day1/Computer.java
@@ -0,0 +1,46 @@
+package day1;
+
+public class Computer {
+    public int year;
+    public double price;
+    public boolean isNotebook;
+    public double hardDiskMemory;
+    public double freeMemory;
+    public String operationSystem;
+    public String brand;
+
+    public Computer (){
+
+    }
+    public Computer(int year, double price, boolean isNotebook, double hardDiskMemory, double freeMemory, String operationSystem) {
+        this.year = year;
+        this.price = price;
+        this.isNotebook = isNotebook;
+        this.hardDiskMemory = hardDiskMemory;
+        this.freeMemory = freeMemory;
+        this.operationSystem = operationSystem;
+    }
+
+    public void changeOperationSystem(String newOperationSystem) {
+        this.operationSystem = newOperationSystem;
+    }
+
+    public void useMemory(double memory) {
+        if (memory > this.freeMemory) {
+            System.out.println("Not enough free memory!");
+        } else {
+            this.freeMemory -= memory;
+        }
+    }
+
+    public String toString() {
+        String result = "";
+        result += "Year: " + this.year + "\n";
+        result += "Price: " + this.price + "\n";
+        result += "Is notebook: " + this.isNotebook + "\n";
+        result += "Hard disk memory: " + this.hardDiskMemory + "\n";
+        result += "Free memory: " + this.freeMemory + "\n";
+        result += "Operation system: " + this.operationSystem + "\n";
+        return result;
+    }
+}
--- src/day1/Welcome.java
+++ src/day1/Welcome.java
@@ -5,12 +5,23 @@ public class Welcome {
     //function to print current date and time
     public static void printDateTime() {
         java.util.Date date = new java.util.Date();
-        System.out.println(date.toString());
+        System.out.println(date);
     }
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
         printDateTime();
+
+        Computer c1 = new Computer();
+        Computer c2 = new Computer();
+
+        c1.brand = "Apple";
+        c1.year = 2018;
+        c1.price = 1000;
+        c1.operationSystem = "MacOS";
+        c1.isNotebook = true;
+        c1.changeOperationSystem("Windows 10");
+
     }
 }
 
