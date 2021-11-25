70d1caeb214e01ce9d75daf389650a53bbf15943
Nirakar Nepal <nnepal@verra.org>
2021-11-24 19:04:27 -0600

    Added Bank and Account class

---
+++ src/day2/Account.java
@@ -0,0 +1,74 @@
+package day2;
+
+public class Account {
+    private int id;
+    private double balance;
+    private String name;
+    private String email;
+    private String phone;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+//    public void setBalance(double balance) {
+//        this.balance = balance;
+//    }
+
+    public void deposit(double amount) {
+        if (amount < 0) {
+            System.out.println("Deposit amount cannot be negative");
+        } else {
+            this.balance += amount;
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > this.balance) {
+            System.out.println("Insufficient funds");
+        } else {
+            this.balance -= amount;
+        }
+    }
+
+    public void printAccount() {
+        System.out.println("Account ID: " + this.id);
+        System.out.println("Account Balance: " + this.balance);
+        System.out.println("Account Holder Name: " + this.name);
+        System.out.println("Account Holder Email: " + this.email);
+        System.out.println("Account Holder Phone: " + this.phone);
+    }
+}
---
+++ src/day2/Bank.java
@@ -0,0 +1,27 @@
+package day2;
+
+public class Bank {
+
+    public static void main(String[] args) {
+        Account a1 = new Account();
+        Account a2 = new Account();
+
+
+        a1.setName("John");
+        a1.setId(1);
+        a1.setPhone("123456789");
+        a1.deposit(100);
+        a1.printAccount();
+        a1.deposit(200);
+        a1.withdraw(50);
+        a1.printAccount();
+
+
+        a2.setName("Marry");
+        a2.setId(2);
+        a2.setPhone("987654321");
+
+
+
+    }
+}
