03bef0dff50258fd3751e6124486239c5ba5ec84
Nirakar Nepal <nnepal@verra.org>
2021-11-30 19:23:04 -0600

    Used Objects Helper Class in Employee

--- src/day5/Employee.java
+++ src/day5/Employee.java
@@ -1,6 +1,7 @@
 package day5;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class Employee {
     private String name;
@@ -10,20 +11,21 @@ public class Employee {
     private String race; //opt
 
     //Default constructor
-    public Employee(){
+    public Employee() {
 
     }
 
     //Parameterized constructor
-    public Employee(String name, double salary, int age, LocalDate hireDate){
-        this.name = name;
+    public Employee(String name, double salary, int age, LocalDate hireDate) {
+        //Objects helper class
+        this.name = Objects.requireNonNullElse(name, "Unknown");
         this.salary = salary;
         this.age = age;
-        this.hireDate = hireDate;
+        this.hireDate = Objects.requireNonNullElse(hireDate, LocalDate.now());
     }
 
     //Overloading
-    public Employee(String name, double salary, int age, LocalDate hireDate, String race){
+    public Employee(String name, double salary, int age, LocalDate hireDate, String race) {
         this.name = name;
         this.salary = salary;
         this.age = age;
@@ -31,45 +33,45 @@ public class Employee {
         this.race = race;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public double getSalary(){
+    public double getSalary() {
         return salary;
     }
 
-    public int getAge(){
+    public int getAge() {
         return age;
     }
 
-    public LocalDate getHireDate(){
+    public LocalDate getHireDate() {
         return hireDate;
     }
 
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
     }
 
-    public void setSalary(double salary){
+    public void setSalary(double salary) {
         this.salary = salary;
     }
 
-    public void setAge(int age){
+    public void setAge(int age) {
         this.age = age;
     }
 
-    public void setHireDate(LocalDate hireDate){
+    public void setHireDate(LocalDate hireDate) {
         this.hireDate = hireDate;
     }
 
     //raise salary
-    public void raiseSalary(double byPercent){
+    public void raiseSalary(double byPercent) {
         double raise = salary * byPercent / 100;
         salary += raise;
     }
 
-    public void printEmployee(){
+    public void printEmployee() {
         System.out.println("Name: " + this.name);
         System.out.println("Salary: " + salary);
         System.out.println("Age: " + age);
