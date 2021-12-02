30ba931b8379251974d6e90605770707e2d24a12
Nirakar Nepal <nnepal@verra.org>
2021-12-01 19:50:24 -0600

    Converted Counter class to Utility/helper class

--- src/day6/Counter.java
+++ src/day6/Counter.java
@@ -1,8 +1,12 @@
 package day6;
 
+//utility class for counting
 public class Counter {
 
     private static int personCount;
+
+    private Counter() {
+    }
 
     public static void increaseCount(){
         personCount++;
--- src/day6/ShoppingMall.java
+++ src/day6/ShoppingMall.java
@@ -3,38 +3,31 @@ package day6;
 public class ShoppingMall {
 
     public static void main(String[] args) {
-        Counter counter1 = new Counter();
-        Counter counter2 = new Counter();
-        Counter counter3 = new Counter();
-        Counter counter4 = new Counter();
 
-        System.out.println(counter1.getCount());
-        System.out.println(counter2.getCount());
-        System.out.println(counter3.getCount());
-        System.out.println(counter4.getCount());
 
-        counter1.increaseCount();
+        System.out.println(Counter.getCount());
+        System.out.println(Counter.getCount());
+        System.out.println(Counter.getCount());
 
-        System.out.println(counter1.getCount());
-        System.out.println(counter2.getCount());
-        System.out.println(counter3.getCount());
-        System.out.println(counter4.getCount());
+        Counter.increaseCount();
 
-        counter2.increaseCount();
-        counter4.increaseCount();
-        counter3.increaseCount();
+        System.out.println(Counter.getCount());
+        System.out.println(Counter.getCount());
 
-        System.out.println(counter1.getCount());
-        System.out.println(counter2.getCount());
-        System.out.println(counter3.getCount());
-        System.out.println(counter4.getCount());
 
-        counter1.decreaseCount();
-        counter2.decreaseCount();
+        Counter.increaseCount();
+        Counter.increaseCount();
+        Counter.increaseCount();
 
-        System.out.println(counter1.getCount());
-        System.out.println(counter2.getCount());
-        System.out.println(counter3.getCount());
-        System.out.println(counter4.getCount());
+        System.out.println(Counter.getCount());
+        System.out.println(Counter.getCount());
+        System.out.println(Counter.getCount());
+
+        Counter.decreaseCount();
+        Counter.decreaseCount();
+
+        System.out.println(Counter.getCount());
+        System.out.println(Counter.getCount());
+        System.out.println(Counter.getCount());
     }
 }
